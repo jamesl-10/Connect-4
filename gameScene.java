d525145c0191c0b55212d0483505dbd29d5fdedf
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-16 12:01:54 -0400

    Update gameScene.java

--- gameScene.java
+++ gameScene.java
@@ -21,250 +21,249 @@ public class gameScene extends JPanel
 	private static int turnNumber = 0;
     private static boolean gameOver = false;
 
-public gameScene(ActionListener action)
-{		
-	this.setLayout(null);
-	JPanel board = new JPanel();  
-	// Makes an 8 x 8 grid
-    board.setLayout(new GridLayout(6, 8));
-    board.setBounds(100, 100, 720, 540);
-    
-	JButton backButton = new JButton();
-	backButton.addActionListener(action);
-	backButton.setBounds(100, 700, 300, 100);
-	backButton.setText("Back");
-    
-	JLabel winnerLabel = new JLabel();
-	winnerLabel.setBounds(500,700,300,100);
-	winnerLabel.setFont(new Font("Arial Unicode MS", Font.PLAIN, 30));
+	public gameScene(ActionListener action)
+	{		
+		this.setLayout(null);
+		JPanel board = new JPanel();  
+		// Makes an 8 x 8 grid
+	    board.setLayout(new GridLayout(6, 8));
+	    board.setBounds(100, 100, 720, 540);
+	    
+		JButton backButton = new JButton();
+		backButton.addActionListener(action);
+		backButton.setBounds(100, 700, 300, 100);
+		backButton.setText("Back");
+	    
+		JLabel winnerLabel = new JLabel();
+		winnerLabel.setBounds(500,700,300,100);
+		winnerLabel.setFont(new Font("Arial Unicode MS", Font.PLAIN, 30));
+	    
+	    Player p1 = new Player("James", "Blue");
+	    Player p2 = new Player("Chloe", "White");
 	
-    
-    Player p1 = new Player("James", "Blue");
-    Player p2 = new Player("Chloe", "White");
-
-    Spot spots[][] = new Spot[8][6];
-    
-    for (int i = 0; i < 6; i++)
-    {
-    	for (int j = 0; j < 8; j++)
-    	{
-    		Spot spot = new Spot(j,i);
-    		spot.setSize(90,90);
-    		spots[j][i] = spot;
-    		board.add(spot);
-    		
-    		try
-    		{
-    		    Image img = ImageIO.read(getClass().getResource("resources/spot.jpg"));
-    		    spot.setIcon(new ImageIcon(img));
-    		     
-    		    spot.addMouseListener(new MouseListener()
-    		    {
-    		    	@Override
-				    public void mouseClicked(MouseEvent e)
-				    {
-				    	// TODO Auto-generated method stub 
-				    }
-
-					@Override
-					public void mouseEntered(MouseEvent e)
-					{
-						// TODO Auto-generated method stub
-					}
-
-					@Override
-					public void mouseExited(MouseEvent e)
-					{
-						// TODO Auto-generated method stub
-					}
-
-					@Override
-					public void mousePressed(MouseEvent e)
-					{
-						// TODO Auto-generated method stub
-						if (!gameOver)
+	    Spot spots[][] = new Spot[8][6];
+	    
+	    for (int i = 0; i < 6; i++)
+	    {
+	    	for (int j = 0; j < 8; j++)
+	    	{
+	    		Spot spot = new Spot(j,i);
+	    		spot.setSize(90,90);
+	    		spots[j][i] = spot;
+	    		board.add(spot);
+	    		
+	    		try
+	    		{
+	    		    Image img = ImageIO.read(getClass().getResource("resources/spot.jpg"));
+	    		    spot.setIcon(new ImageIcon(img));
+	    		     
+	    		    spot.addMouseListener(new MouseListener()
+	    		    {
+	    		    	@Override
+					    public void mouseClicked(MouseEvent e)
+					    {
+					    	// TODO Auto-generated method stub 
+					    }
+	
+						@Override
+						public void mouseEntered(MouseEvent e)
 						{
-							if (spot.getCoin() == null)
+							// TODO Auto-generated method stub
+						}
+	
+						@Override
+						public void mouseExited(MouseEvent e)
+						{
+							// TODO Auto-generated method stub
+						}
+	
+						@Override
+						public void mousePressed(MouseEvent e)
+						{
+							// TODO Auto-generated method stub
+							if (!gameOver)
 							{
-								String coinColor = "";
-								Image img = null;
-								
-								if (turnNumber%2 == 1)
+								if (spot.getCoin() == null)
 								{
-									coinColor = p1.getColor();
-									try
-									{
-										img = ImageIO.read(getClass().getResource("resources/blue.jpg"));
-									}
-									catch (IOException e1)
-									{
-										// TODO Auto-generated catch block
-										e1.printStackTrace();
-									}
-								}
-								else
-								{
-									coinColor = p2.getColor();
+									String coinColor = "";
+									Image img = null;
 									
-									try
+									if (turnNumber%2 == 1)
 									{
-										img = ImageIO.read(getClass().getResource("resources/red.jpg"));
-									}
-									catch (IOException e1)
-									{
-										// TODO Auto-generated catch block
-										e1.printStackTrace();
-									}
-								}
-								
-								for (int i = 0; i < spots.length; i++)
-								{
-									if (spots[spot.getXPos()][5 - i].getCoin() == null)
-									{
-										Coin c = new Coin(coinColor);
-										spots[spot.getXPos()][5 - i].addCoin(c);
-										spots[spot.getXPos()][5 - i].setIcon(new ImageIcon(img));
-										break;
-									}
-								}
-								
-								for(int i = 0; i < spots[0].length; i++)
-								{
-									for (int j = 0;  j < spots.length; j++)
-									{
-										if (hasWinner(spots,spots[j][i]) == true)
+										coinColor = p1.getColor();
+										try
 										{
-											String winner =  "";
-											
-											if (turnNumber%2 == 0)
-											{
-												winner = p1.getName();
-											}
-											else
-											{
-												winner = p2.getName();
-											}
-											
-											winnerLabel.setText("The winner is " + winner);
-											gameOver = true;
+											img = ImageIO.read(getClass().getResource("resources/blue.jpg"));
+										}
+										catch (IOException e1)
+										{
+											// TODO Auto-generated catch block
+											e1.printStackTrace();
 										}
 									}
+									else
+									{
+										coinColor = p2.getColor();
+										
+										try
+										{
+											img = ImageIO.read(getClass().getResource("resources/red.jpg"));
+										}
+										catch (IOException e1)
+										{
+											// TODO Auto-generated catch block
+											e1.printStackTrace();
+										}
+									}
+									
+									for (int i = 0; i < spots.length; i++)
+									{
+										if (spots[spot.getXPos()][5 - i].getCoin() == null)
+										{
+											Coin c = new Coin(coinColor);
+											spots[spot.getXPos()][5 - i].addCoin(c);
+											spots[spot.getXPos()][5 - i].setIcon(new ImageIcon(img));
+											break;
+										}
+									}
+									
+									for(int i = 0; i < spots[0].length; i++)
+									{
+										for (int j = 0;  j < spots.length; j++)
+										{
+											if (hasWinner(spots,spots[j][i]) == true)
+											{
+												String winner =  "";
+												
+												if (turnNumber%2 == 0)
+												{
+													winner = p1.getName();
+												}
+												else
+												{
+													winner = p2.getName();
+												}
+												
+												winnerLabel.setText("The winner is " + winner);
+												gameOver = true;
+											}
+										}
+									}
+									
+									turnNumber += 1;
 								}
-								
-								turnNumber += 1;
 							}
 						}
-					}
-
-					@Override
-					public void mouseReleased(MouseEvent arg0)
-					{
-						// TODO Auto-generated method stub
-							
-					}
-				});
-	    	}
-	    		
-	    	catch (Exception e) {}
-	    		
-	    	spots[j][i] = spot;
+	
+						@Override
+						public void mouseReleased(MouseEvent arg0)
+						{
+							// TODO Auto-generated method stub
+								
+						}
+					});
+		    	}
+		    		
+		    	catch (Exception e) {}
+		    		
+		    	spots[j][i] = spot;
+		    }
 	    }
-    }
-    
-    this.add(board);
-    this.add(backButton);
-    this.add(winnerLabel);
- } 
-   
- public boolean hasWinner(Spot[][] spots, Spot s)
-	{
-		if (s.getCoin() != null)
+	    
+	    this.add(board);
+	    this.add(backButton);
+	    this.add(winnerLabel);
+	 } 
+	   
+	 public boolean hasWinner(Spot[][] spots, Spot s)
 		{
-			// Horizontal check
-			try
+			if (s.getCoin() != null)
 			{
-				for (int i = 0; i < 4; i++)
+				// Horizontal check
+				try
 				{
-					if (spots[s.getXPos() - i][s.getYPos()].getCoin().getColor().equals(s.getCoin().getColor()))
+					for (int i = 0; i < 4; i++)
 					{
-						if (i == 3)
+						if (spots[s.getXPos() - i][s.getYPos()].getCoin().getColor().equals(s.getCoin().getColor()))
 						{
-							return true;
+							if (i == 3)
+							{
+								return true;
+							}
+						}
+						
+						else
+						{
+							break;
 						}
 					}
-					
-					else
+				}
+				catch (Exception e) {}
+				
+				// Vertical check
+				try
+				{
+					for (int i = 0; i < 4; i++)
 					{
-						break;
+						if (spots[s.getXPos()][s.getYPos() - i].getCoin().getColor().equals(s.getCoin().getColor()))
+						{
+							if (i == 3)
+							{
+								return true;
+							}
+						}
+						
+						else
+						{
+							break;
+						}
 					}
 				}
+				catch (Exception e) {}
+				
+				// Diagonal check right
+				try
+				{
+					for (int i = 0; i < 4; i++)
+					{
+						if (spots[s.getXPos() - i][s.getYPos() + i].getCoin().getColor().equals(s.getCoin().getColor()))
+						{
+							if (i == 3)
+							{
+								return true;
+							}
+						}
+						
+						else
+						{
+							break;
+						}
+					}
+				}
+				catch (Exception e) {}
+				
+				// Diagonal check left
+				try
+				{
+					for (int i = 0; i < 4; i++)
+					{
+						if (spots[s.getXPos() + i][s.getYPos() + i].getCoin().getColor().equals(s.getCoin().getColor()))
+						{
+							if (i == 3)
+							{
+								return true;
+							}
+						}
+						
+						else
+						{
+							break;
+						}
+					}
+				}
+				catch (Exception e) {}
 			}
-			catch (Exception e) {}
 			
-			// Vertical check
-			try
-			{
-				for (int i = 0; i < 4; i++)
-				{
-					if (spots[s.getXPos()][s.getYPos() - i].getCoin().getColor().equals(s.getCoin().getColor()))
-					{
-						if (i == 3)
-						{
-							return true;
-						}
-					}
-					
-					else
-					{
-						break;
-					}
-				}
-			}
-			catch (Exception e) {}
-			
-			// Diagonal check right
-			try
-			{
-				for (int i = 0; i < 4; i++)
-				{
-					if (spots[s.getXPos() - i][s.getYPos() + i].getCoin().getColor().equals(s.getCoin().getColor()))
-					{
-						if (i == 3)
-						{
-							return true;
-						}
-					}
-					
-					else
-					{
-						break;
-					}
-				}
-			}
-			catch (Exception e) {}
-			
-			// Diagonal check left
-			try
-			{
-				for (int i = 0; i < 4; i++)
-				{
-					if (spots[s.getXPos() + i][s.getYPos() + i].getCoin().getColor().equals(s.getCoin().getColor()))
-					{
-						if (i == 3)
-						{
-							return true;
-						}
-					}
-					
-					else
-					{
-						break;
-					}
-				}
-			}
-			catch (Exception e) {}
-		}
-		
-		return false;
-	} 
-}
+			return false;
+		} 
+	}
