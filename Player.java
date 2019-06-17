980cfde9b2c2499596394ad23e3e44a3a9d74958
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-16 21:07:29 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,20 +1,35 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: June 2019
+ * Created for: ICS4U
+ * Connect 4 code, player class
+ * 
+ * Player class for main game
+ * 
+ ****************************************************************************/
+
 class Player
 {
+	// Properties
 	private String name;
-	private String color;
+	private COLORS color;
 	
-	public Player(String name, String color)
+	// Constructor
+	public Player(String name, COLORS color)
 	{
 		this.name = name;
 		this.color = color;
 	}
 	
+	// Return name
 	public String getName()
 	{
 		return name;
 	}
 	
-	public String getColor()
+	// Return color
+	public COLORS getColor()
 	{
 		return color;
 	}
