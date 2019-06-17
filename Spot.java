f46ee95fcbcf134375c4e936bbc94f5951d59ecc
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-16 21:08:18 -0400

    Update Spot.java

--- Spot.java
+++ Spot.java
@@ -1,7 +1,19 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: June 2019
+ * Created for: ICS4U
+ * Connect 4 code, spot class
+ * 
+ * Every spot on the game board
+ * 
+ ****************************************************************************/
+
 import javax.swing.JButton;
 
 class Spot extends JButton
 {
+	// Properties
 	private Coin coin;
 	private int xPos;
 	private int yPos;
@@ -13,21 +25,25 @@ class Spot extends JButton
 		this.yPos = yPos;
 	}
 	
+	// Adds coin
 	public void addCoin(Coin c)
 	{
 		this.coin = c;
 	}
 	
+	// Returns coin
 	public Coin getCoin()
 	{
 		return this.coin;
 	}
 	
+	// Returns x-position
 	public int getXPos()
 	{
 		return xPos;
 	}
 	
+	// Returns y-position
 	public int getYPos()
 	{
 		return yPos;
