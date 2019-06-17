3ca740f2f4b13362af7d03bba612c9e9923e356d
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-16 21:09:48 -0400

    Update mainMenuScene.java

--- mainMenuScene.java
+++ mainMenuScene.java
@@ -1,38 +1,39 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.*;
+/****************************************************************************
+ * Created by: Shuvaethy Neill
+ * Created on: June 2019
+ * Created for: ICS4U
+ * This program is the main menu scene
+ ****************************************************************************/
 
-import javax.imageio.ImageIO;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class mainMenuScene extends JPanel
 {
+	// Constructor
+	// Actions go to credits scene and game scene
 	public mainMenuScene(ActionListener credits, ActionListener game)
 	{    
 		this.setLayout(null);
 	    
+		// Adds new play button that goes to game scene
 	    JButton playButton = new JButton();
-	    playButton.setBounds(900, 600, 200, 50);
+	    playButton.setBounds(900, 700, 200, 50);
 	    playButton.setText("Play");
 	    this.add(playButton);
-	    
 	    playButton.addActionListener(game);
 	    
+	    // Adds new credits button that goes to credits scene
 	    JButton creditsButton = new JButton();
-	    creditsButton.setBounds(1500, 750, 150, 50);
+	    creditsButton.setBounds(925, 800, 150, 50);
 	    creditsButton.setText("Credits");
 	    this.add(creditsButton);
-	    
 	    creditsButton.addActionListener(credits);
 	    
+	    // Background image
 		JLabel picLabel = new JLabel();
 		picLabel.setIcon(new ImageIcon(this.getClass().getResource("resources/goodMainMenu.jpg")));
-		picLabel.setBounds(0, 0, 2000, 1000);
+		picLabel.setBounds(0, 0, 1900, 1000);
 		this.add(picLabel);
 		
 	    this.setVisible(true);
